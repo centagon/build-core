e35f81f28d81fb0df69916ffd6972c29ab4fa045
Jelle Spekken <jelle.spekken@centagon.com>
2016-11-08 10:10:29 +0100

    inline color styles added through the color service provider

---
+++ src/Build/Core/Providers/ColorServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Build\Core\Providers;
+
+/*
+ * This file is part of the Build package.
+ *
+ * (c) Centagon <contact@centagon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Build\Core\Eloquent\Models\Color;
+use Build\Core\Support\Facades\Asset;
+use Illuminate\Support\Facades\Cache;
+
+class ColorServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+
+    /**
+     * Boot the provider.
+     */
+    public function boot()
+    {
+        if (! app()->runningInConsole()) {
+            $this->renderInlineStyles();
+        }
+    }
+
+    protected function renderInlineStyles()
+    {
+        $styles = [];
+
+        $colors = Cache::rememberForever('build.colors.all', function () {
+            return Color::all();
+        });
+
+        foreach ($colors as $color) {
+            $styles[] = '.background-' . $color->name . '{background-color:' . $color->color . ';}';
+            $styles[] = '.foreground-' . $color->name . '{color:' . $color->color . ';}';
+        }
+
+        Asset::inlineStyle(implode('', $styles));
+    }
+}
--- src/Build/Core/ServiceProvider.php
+++ src/Build/Core/ServiceProvider.php
@@ -15,6 +15,7 @@ use Build\Core\Support\Context;
 use Build\Core\Http\Routing\Discovery;
 use Build\Core\Providers\MenuServiceProvider;
 use Build\Core\Providers\AlertServiceProvider;
+use Build\Core\Providers\ColorServiceProvider;
 use Build\Core\Providers\RouteServiceProvider;
 use Build\Core\Providers\AssetServiceProvider;
 use Build\Core\Providers\PolicyServiceProvider;
@@ -77,6 +78,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->app->register(ConsoleServiceProvider::class);
         $this->app->register(ModulesServiceProvider::class);
         $this->app->register(PolicyServiceProvider::class);
+        $this->app->register(ColorServiceProvider::class);
         $this->app->register(AlertServiceProvider::class);
         $this->app->register(AssetServiceProvider::class);
         $this->app->register(RouteServiceProvider::class);
