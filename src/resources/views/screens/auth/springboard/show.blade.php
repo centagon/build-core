3545f5f1d860abb9f16078057044045f15a04878
Jelle Spekken <jspekken@gmail.com>
2016-11-09 21:32:57 +0100

    added listgroup elements styles [skip ci]

--- src/resources/views/screens/auth/springboard/show.blade.php
+++ src/resources/views/screens/auth/springboard/show.blade.php
@@ -3,20 +3,24 @@
 @section('content')
 
     <div class="row">
-        <div class="small-12 medium-5 medium-centered columns">
+        <div class="small-12 medium-4 medium-push-4">
 
-                <div class="callout">
+            <section class="branding-bar">
+                <section class="title">
+                    <h3>Welcome {{ auth()->user()->name }}</h3>
+                    <p>Please choose a website to login to.</p>
+                </section>
+            </section>
 
-                    <ul>
-                        @foreach ($websites as $website)
-                            <li>
-                                <a href="{{ route('admin.springboard.open', $website->getKey()) }}">
-                                    {{ $website->name }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
+            <ul class="list-group">
+                @foreach ($websites as $website)
+                    <li>
+                        <a href="{{ route('admin.springboard.open', $website->getKey()) }}" class="list-group__item">
+                            {{ $website->name }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
 
         </div>
     </div>
