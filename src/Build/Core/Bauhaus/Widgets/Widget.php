8fe69a0ffec963294420158af061d12280d904d7
Jelle Spekken <jspekken@gmail.com>
2016-11-09 22:16:38 +0100

    massive cleanup for the widget attribute render method

--- src/Build/Core/Bauhaus/Widgets/Widget.php
+++ src/Build/Core/Bauhaus/Widgets/Widget.php
@@ -21,28 +21,37 @@ use Illuminate\Support\HtmlString;
 abstract class Widget extends Mapper
 {
 
+    /**
+     * Holds the widget attributes.
+     * @var array
+     */
     protected $attributes = [];
 
     public function renderedAttributes()
     {
         return implode(' ', array_map(function ($key, $value) {
+            // Does the string start with a colon? That means that
+            // we want to get the value set by the builder and
+            // will not provide one on our own.
             if (starts_with($value, ':')) {
                 $value = $this->get($key);
             }
 
-            if (! starts_with($value, '@')) {
-                $string = sprintf('%s=%s', $key, $value);
-            } else {
+            // When the string starts with an at (@) symbol that means
+            // that we want to render/modify the attribute ourselfs.
+            if (starts_with($value, '@')) {
                 $modifier = 'get' . studly_case(substr($value, 1)) . 'Modifier';
 
+                // Can we call the modifier method on the field?
                 if (! method_exists($this, $modifier)) {
                     throw new \InvalidArgumentException("Cannot reach the `$modifier` modifier.");
                 }
 
-                $string = $this->{$modifier}();
+                return $this->{$modifier}();
             }
 
-            return $string;
+            // Just return the key value pair (html style).
+            return sprintf('%s=%s', $key, $value);
         }, array_keys($this->attributes), $this->attributes));
     }
 }
