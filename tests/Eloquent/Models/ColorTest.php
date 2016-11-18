9addf1ef26caf5583e3fda4fb2f3a67d158f6a7d
Jelle Spekken <jelle.spekken@centagon.com>
2016-11-18 11:29:15 +0100

    updated the color test

--- tests/Eloquent/Models/ColorTest.php
+++ tests/Eloquent/Models/ColorTest.php
@@ -19,9 +19,42 @@ class ColorTest extends AbstractTestCase
 
     public function test_can_create_color()
     {
-        $color = new Color(['name' => 'test', 'color' => 'red']);
+        $color = $this->getColor();
         $color->save();
 
-        $this->assertTrue(true);
+        $this->assertEquals($color->name, 'my-dark-color');
+        $this->assertEquals($color->color, '#000000');
+    }
+
+    public function test_can_convert_to_hex()
+    {
+        $color = $this->getColor();
+        $color->save();
+
+        $this->assertEquals($color->hex_color, '#000000');
+    }
+
+    public function test_can_convert_to_rgb()
+    {
+        $color = $this->getColor();
+        $color->save();
+
+        $this->assertEquals($color->rgb_color, 'rgb(0, 0, 0)');
+    }
+
+    public function test_can_get_best_contrast()
+    {
+        $color = $this->getColor();
+        $color->save();
+
+        $this->assertEquals($color->best_contrast, 'white');
+    }
+
+    protected function getColor()
+    {
+        return new Color([
+            'name' => 'My Dark Color',
+            'color' => '#000000',
+        ]);
     }
 }
