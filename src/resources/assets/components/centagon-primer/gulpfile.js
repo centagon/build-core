6e0f18cfc2a8da0049ba2ab948a05a02f6c95852
Jelle Spekken <jelle.spekken@centagon.com>
2016-11-28 09:38:28 +0100

    components directory removed

--- src/resources/assets/components/centagon-primer/gulpfile.js
+++
@@ -1,9 +0,0 @@
-const elixir = require('laravel-elixir');
-
-elixir.config.assetsPath = 'src';
-elixir.config.publicPath = 'dist';
-
-elixir(mix => {
-    mix.sass('primer.scss');
-    mix.browserify('primer.js');
-});
