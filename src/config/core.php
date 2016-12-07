b8cd89a816f25cf29100ff5fdd67295552b743b6
Jelle Spekken <jelle.spekken@centagon.com>
2016-12-07 12:29:57 +0100

    e-tag middleware removed

--- src/config/core.php
+++ src/config/core.php
@@ -35,7 +35,6 @@ return [
 
     'middleware' => [
 
-        'etag' => Build\Core\Http\Middleware\Etag::class,
         'installed' => Build\Core\Http\Middleware\Installed::class,
         'scope-state' => Build\Core\Http\Middleware\ScopeState::class,
         'springboard' => Build\Core\Http\Middleware\Springboard::class,
