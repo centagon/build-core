61b55fcadedcdc62fd9ebf9a9eb7273c7cd714d9
Jelle Spekken <jelle.spekken@centagon.com>
2016-12-27 09:08:46 +0100

    updated rotues

---
+++ src/routes/async/guarded/assets.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the Build package.
+ *
+ * (c) Centagon <contact@centagon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+Route::get('assets/fetch-groups', 'AssetsController@fetchGroups');
+Route::get('assets/fetch-websites', 'AssetsController@fetchWebsites');
+Route::resource('assets', 'AssetsController');
+
