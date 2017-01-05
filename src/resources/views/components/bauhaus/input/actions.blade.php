ee7a0aa09829a968a2ea0eddbf12428bc8fe748d
René Heijnemans <rene.heijnemans@centagon.com>
2017-01-05 15:48:24 +0100

    actions : change buttin--secondary to button--secondary

--- src/resources/views/components/bauhaus/input/actions.blade.php
+++ src/resources/views/components/bauhaus/input/actions.blade.php
@@ -4,7 +4,7 @@
 			<button class="button button--success">
 				{{ $node->get('success-label', 'Save changes') }}
 			</button>
-			<a class="button buttin--secondary" href="{{ $node->get('cancel-url') ? : app('url')->previous() }}">
+			<a class="button button--secondary" href="{{ $node->get('cancel-url') ? : app('url')->previous() }}">
 				{{ $node->get('cancel-label', 'Cancel') }}
 			</a>
 		</div>
