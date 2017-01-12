3fb3c5b48af78ee414e44bd7b9360b84673b432f
Jelle Spekken <jelle.spekken@centagon.com>
2017-01-12 12:10:52 +0100

    alignment fixes

--- src/resources/views/components/bauhaus/input/select.blade.php
+++ src/resources/views/components/bauhaus/input/select.blade.php
@@ -8,31 +8,31 @@
 
 		@if ( $node->isMultiple() )
 			<select name="{{ $node->get('name') }}[]" id="f-{{ $node->get('name') }}" {!! $node->getData()  !!} {!! $node->getAttributes()  !!} multiple>
-				@else
-					<select name="{{ $node->get('name') }}" id="f-{{ $node->get('name') }}" {!! $node->getData()  !!} {!! $node->getAttributes()  !!} >
-						<option value="">Select an option</option>
-						@endif
+		@else
+			<select name="{{ $node->get('name') }}" id="f-{{ $node->get('name') }}" {!! $node->getData()  !!} {!! $node->getAttributes()  !!} >
+				<option value="">Select an option</option>
+		@endif
 
-						@if( $node->isMultiple() )
-							@foreach ($node->getOptions() as $key => $value)
-								@if ( array_get( $node->getOld(), $key ) )
-									<option value="{{ $key }}" selected>{{ $value }}</option>
-								@else
-									<option value="{{ $key }}">{{ $value }}</option>
-								@endif
-							@endforeach
-						@else
-							@foreach ($node->getOptions() as $key => $value)
-								@if ( $node->getOld() == $key )
-									<option value="{{ $key }}" selected>{{ $value }}</option>
-								@else
-									<option value="{{ $key }}">{{ $value }}</option>
-								@endif
-							@endforeach
-						@endif
+			@if( $node->isMultiple() )
+				@foreach ($node->getOptions() as $key => $value)
+					@if ( array_get( $node->getOld(), $key ) )
+						<option value="{{ $key }}" selected>{{ $value }}</option>
+					@else
+						<option value="{{ $key }}">{{ $value }}</option>
+					@endif
+				@endforeach
+			@else
+				@foreach ($node->getOptions() as $key => $value)
+					@if ( $node->getOld() == $key )
+						<option value="{{ $key }}" selected>{{ $value }}</option>
+					@else
+						<option value="{{ $key }}">{{ $value }}</option>
+					@endif
+				@endforeach
+			@endif
 
-					</select>
+		</select>
 
-			@include('build.core::components.bauhaus.partials.field-error')
+		@include('build.core::components.bauhaus.partials.field-error')
 	</div>
 </div>
