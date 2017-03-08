d3d122d31c08535134d7d972d6395d57d245fe0c
René Heijnemans <rene.heijnemans@centagon.com>
2017-03-08 14:40:37 +0100

    reset current value when removing an item from the list adds a name property to indicate that the component should also render hidden inputs

--- src/resources/assets/js/components/input/TagSelect.vue
+++ src/resources/assets/js/components/input/TagSelect.vue
@@ -1,17 +1,18 @@
 <template>
-    <div>
+    <div class="tag-select">
 
         <select v-model="selected" v-on:change="onChange">
             <option value="0" disabled>-- select an option--</option>
-            <option v-for="option in options" :value="option.id">
+            <option v-for="option in options" :value="option.id" v-bind:disabled="isSelected(option)">
                 {{ option.name }}
             </option>
         </select>
 
-        <ul>
+        <ul class="tag-select__selected">
             <li v-for="option in selected_options">
+                <input v-if="name" type="hidden" v-bind:name="name" v-bind:value="option.id"/>
                 {{ option.name }}
-                <button v-on:click="remove(option)">x</button>
+                <span class="tag-select__selected__remove" v-on:click="remove(option)">x</span>
             </li>
         </ul>
 
@@ -23,39 +24,67 @@
     export default {
 
         props: [
+            'name',
             'options',
             'value'
         ],
 
         mounted: function () {
-            this.selected_options = this.value;
+            
+            // Map the values to the id's when we only get an array
+            if (this.value.length) {
+                
+                _.each(this.value, (value) => {
+                    if (value.id) {
+                        this.addSelectedOption(value.id);
+                    } else {
+                        this.addSelectedOption(value);
+                    }
+                });
+            }
         },
 
         data: function () {
             return {
-                options: [],
                 selected_options: [],
                 selected: null
-            }
+            };
         },
-
+        
         methods: {
-            onChange: function () {
+            addSelectedOption(id) {
+                
                 const options = this.options;
 
                 for (let i = 0; i < options.length; i++) {
                     let option = options[i];
 
-                    if (option.id == this.selected) {
+                    if (option.id == id) {
                         this.selected_options.push(option);
                     }
                 }
 
+            },
+            
+            isSelected(check) {
+                let selected = false;
+                _.each(this.selected_options, (option) => {
+                    if (option.id === check.id) {
+                        selected = true;
+                        return false;
+                    }
+                });
+                return selected;
+            },
+    
+            onChange: function () {
+                this.addSelectedOption(this.selected);
                 this.$emit('input', this.selected_options);
             },
 
             remove: function (selected) {
                 this.selected_options.splice(selected, 1);
+                this.selected = null;
             }
         }
     }
